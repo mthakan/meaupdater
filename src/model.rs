5786d01c0175c4c53b8c8765b2c3e6edc6d5d938
mthakan <measoftmete@outlook.com>
2025-06-28 11:11:49 +0300

    Create model.rs

---
+++ src/model.rs
@@ -0,0 +1,15 @@
+// src/model.rs
+#[derive(Debug, PartialEq)]
+pub enum UpdateType {
+    Security,
+    Software,
+    Unknown,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct PackageUpdate {
+    pub name: String,
+    pub current_version: String,
+    pub new_version: String,
+    pub update_type: UpdateType,
+}
