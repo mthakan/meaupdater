ff59959942cb9dec7832706889180bf27ab87992
mthakan <measoftmete@outlook.com>
2025-07-07 04:53:42 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,8 @@ pub mod policy;
 pub mod progress;
 pub mod repo_manager;
 pub mod repo_window;
+pub mod kernel_manager;
+pub mod kernel_window;
+pub mod driver_manager;
+pub mod driver_window;
+pub mod driver_progress;
